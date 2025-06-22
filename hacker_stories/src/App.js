058babcab4a2780f74946cdc7540d13d84cd867a
Samuel Birhanu <samibre121@gmail.com>
2025-06-22 09:30:25 +0300

    using variables embedded in jsx to be displayed

--- hacker_stories/src/App.js
+++ hacker_stories/src/App.js
@@ -2,15 +2,25 @@ import './App.css';
 
 let numClicks = 0
 
+function greet(name){
+  return "hello, " + name
+}
+
 function App() {
   
   numClicks++
+
+  const welcome = {
+    greeting: 'hey',
+    title: 'react'
+  }
   
   const titile = "you clicked it " + numClicks + " times"
   return (
     <div>
-      <h1>Hello world!</h1>
-      <h2>{titile}</h2>
+      <h1>{welcome.greeting}, {greet(welcome.title)}</h1>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='number'/>
     </div>
   );
 }
